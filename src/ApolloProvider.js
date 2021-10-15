2185600c1e77fc0437ed428107b077061a27bb3b
Parag <paragbokde51@gmail.com>
2021-10-15 22:25:13 +0530

    finale

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -3,37 +3,20 @@ import ApolloClient from 'apollo-client'
 import { ApolloProvider } from '@apollo/react-hooks'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { setContext } from 'apollo-link-context'
-import { getMainDefinition } from '@apollo/client/utilities'
-import { split, HttpLink } from '@apollo/client'
-import { WebSocketLink } from '@apollo/client/link/ws'
+import { createHttpLink } from 'apollo-link-http'
 
 import App from './App'
 
 const token = localStorage.getItem('jwtToken')
 
-const httpLink = new HttpLink({
-    uri: `https://damp-headland-47501.herokuapp.com/graphql`
+const httpLink = createHttpLink({
+    uri: `https://damp-headland-47501.herokuapp.com/`
 })
 
 const authLink = setContext(() => ({ headers: { Authorization: token ? 'Bearer ' + token : '' }}))
 
-
-const wsLink = new WebSocketLink({
-    uri: 'wss://damp-headland-47501.herokuapp.com/graphql', options: { reconnect: true, connectionParams: { authToken: token ? 'Bearer ' + token : '' }}
-})
-
-const link = split(
-    ({ query }) => {
-        const definition = getMainDefinition(query)
-        
-        return (
-            definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
-        )
-    }, wsLink, authLink.concat(httpLink)
-)
-
 const client = new ApolloClient({
-    link, cache: new InMemoryCache()
+    link: authLink.concat(httpLink), cache: new InMemoryCache()
 })
 
 export default (
