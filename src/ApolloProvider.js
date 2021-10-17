ccc1ddd3756baf0c8b558adbc0c5371b65fa5c71
Parag <paragbokde51@gmail.com>
2021-10-17 16:14:04 +0530

    fixed authentication issue

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,13 +7,14 @@ import { createHttpLink } from 'apollo-link-http'
 
 import App from './App'
 
-const token = localStorage.getItem('jwtToken')
-
 const httpLink = createHttpLink({
     uri: `https://damp-headland-47501.herokuapp.com/`
 })
 
-const authLink = setContext(() => ({ headers: { Authorization: token ? 'Bearer ' + token : '' }}))
+const authLink = setContext(() => {
+    const token = localStorage.getItem('jwtToken')
+    return { headers: { authorization: token ? 'Bearer ' + token : '' }}
+})
 
 const client = new ApolloClient({
     link: authLink.concat(httpLink), cache: new InMemoryCache()
